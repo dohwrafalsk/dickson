fe0ee1d76beecd31599508677b633865d1666dd6
dohwrafalsk <wrafalski@health.nyc.gov>
2015-09-17 15:07:34 -0400

    Update Device.java

--- Device.java
+++ Device.java
@@ -1,24 +1,32 @@
-import java.util.List;
+
+
+
 import java.util.LinkedList;
+import java.util.List;
+
 
 
 public class Device
 {
-  private String token;
-  private List<Channel> channels;
+  private final String token;
+  private final List<Channel> channels;
 
 
   public Device(String token)
   {
     this.token = token;
-    channels = new LinkedList<Channel>();
+    channels = new LinkedList<>();
   }
 
 
-  public String getToken() { return token; }
+  public String getToken() { 
+      return token; 
+  }
 
 
-  public List<Channel> getChannels() { return channels; }
+  public List<Channel> getChannels() {
+      return channels; 
+  }
 
 
   public void addChannel(Channel channel)
